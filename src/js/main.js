5731c9e678df6c314c0d61b975b7aa643c5ca3f7
Irene Mezquita <irenemezyar@gmail.com>
2023-02-24 11:13:59 +0100

    reset button

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
 // Add HTML variables
 const searchText = document.querySelector('.js-searchText');
 const searchButton = document.querySelector('.js-searchButton');
-const searchReset = document.querySelector('.js-searchReset');
+const resetButton = document.querySelector('.js-resetButton');
 const cocktailList = document.querySelector('.js-cocktailList');
 const cocktailFavourites = document.querySelector('.js-cocktailFavourites');
 let url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
@@ -22,29 +22,26 @@ fetch(url)
     renderCocktailList(cocktailsArray);}
   );
 
+// --------------FUNCTIONS--------------
 function renderCocktailList(cocktailsArray) {
-  console.log('Hola');
   cocktailList.innerHTML = '';
   for (const cocktail of cocktailsArray){
     cocktailList.innerHTML += renderCocktails(cocktail); }
   // addEventToPalette();
-  console.log('Hola');
 }
-
 // Function: add li-s to the list ul
 function renderCocktails(cocktail){
-  let html=`<li>
+  let html=`<li class="cocktailLiStyle">
         <article>
-            <img src="${cocktail.strDrinkThumb}" alt="Cocktail picture">
+            <img src="${cocktail.strDrinkThumb}" alt="Cocktail picture" class="cocktailImgStyle">
             <h3 class="cocktailTitle">${cocktail.strDrink}</h3>
         </article>
     </li> `;
   return html;
 }
-
+// Search button
 function handleSearch(event){
   event.preventDefault();
-
   url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText.value}`;
 
   fetch(url)
@@ -55,8 +52,25 @@ function handleSearch(event){
       cocktailsArray = data.drinks;
       renderCocktailList(cocktailsArray);}
     );
-
+}
+// Reset button
+function handleReset(){
+  console.log('hola 1');
+  url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
+  console.log('hola 2');
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+    // Add object elements from API
+      console.log(data);
+      cocktailsArray = data.drinks;
+      renderCocktailList(cocktailsArray);}
+    );
+  searchText.value = '';
+  console.log('hola');
 }
 
+// --------------EVENTS--------------
 searchButton.addEventListener('click', handleSearch);
+resetButton.addEventListener('click', handleReset);
 
